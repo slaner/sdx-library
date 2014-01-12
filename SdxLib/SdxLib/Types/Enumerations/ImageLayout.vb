a465b63e0a1fa5ab864797f61c0c1d26a5207a9a
slaner <black_scv@naver.com>
2014-01-12 23:55:50 +0900

    yoohhohohohohoh

---
+++ SdxLib/SdxLib/Types/Enumerations/ImageLayout.vb
@@ -0,0 +1,21 @@
+﻿''' <summary>
+''' 이미지를 표시하는 방법을 정의합니다.
+''' </summary>
+Public Enum ImageLayout
+
+    ''' <summary>
+    ''' 원래 크기대로 표시합니다.
+    ''' </summary>
+    [Default] = 0
+
+    ''' <summary>
+    ''' 출력되는 개체의 크기에 맞춰서 표시합니다.
+    ''' </summary>
+    Stretch = 1
+
+    ''' <summary>
+    ''' 출력되는 개체의 중앙에 맞춰서 표시합니다.
+    ''' </summary>
+    Center = 2
+
+End Enum
